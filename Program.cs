315b2dc6bbe3acdf48c98b951e677b2c1d108b4a
AzizbekSharifovich <azizbek0698@gmail.com>
2023-07-24 16:48:52 +0500

    Add C# Project file

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ismingizni kiriting:");
+            string ism = Console.ReadLine();
+
+            Console.WriteLine("Yoshingizni kiriting:");
+            int yosh = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Yashash joyingizni kiriting:");
+            string joy = Console.ReadLine();
+
+            Console.WriteLine($"Sizning ismingiz: {ism}");
+            Console.WriteLine($"Sizning yoshingiz: {yosh}");
+            Console.WriteLine($"Siz {joy} da yashaysiz.");
+        }
+    }
+}
+
